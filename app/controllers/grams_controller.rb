fcd020f5568f294ad7119e89fc1b19682d5455e5
Brenton Hughes <brenton.hughes@gmail.com>
2016-09-15 19:02:58 +0000

    Installed simple_form, added new gram form, added gram creation functionality, created gram#create test

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -8,7 +8,18 @@ class GramsController < ApplicationController
   end
 
   def create
+    @gram = Gram.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
   end
 
 end
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -14,4 +14,13 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'grams#create action' do
+    it 'should successfully create a new gram in db' do
+      post :create, params: { gram: { message: 'Hello!' } }
+      expect(response).to redirect_to root_path
+      gram = Gram.last
+      expect(gram.message).to eq('Hello!')
+    end
+  end
 end
