4010c5295bb681d5a8e07ceaacfcda87887e3f07
Brenton Hughes <brenton.hughes@gmail.com>
2016-09-21 21:31:58 +0000

    Fixed tests to work with image validation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ RSpec.configure do |config|
   # Include Devise test helpers
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  include ActionDispatch::TestProcess
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
